d5c6e27cc48cb73da82f2218bc97887fce4e7610
Ritu Gala <ritugala13@gmail.com>
2020-08-17 14:26:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-class node:
-  def __init__(name=none, floor=0, dept=none):
+class Node:
+  def __init__(self, name=None, floor=0, dept=None):
     self.name = name
     self.floor = floor
     self.dept = dept
 
-#n=
-#m=
+# Constraint: Each cell must have at most one node.
+n= 300 # 600m/2 not fixed
+m= 75 # 150m/2 not fixed
+
+arr = [Node() for i in range(m) for j in range(n)]
+
+# Create array with req nodes/corridors/blah
+
+
+currNode = Node()
+#initialize node with current location
+
+
+# Receive input from Alexa about where user wants to go
+dest = None
+
+# Apply A* to get closest dist.
+
+
+
