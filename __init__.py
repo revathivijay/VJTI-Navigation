eeec7c7f48c1e33c4185895b601de4069546560c
VidhiRambhia <vsrambhia@gmail.com>
2020-08-27 21:26:23 +0530

    Merging initialization code

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import csv
+import json 
+from Node import Node
 
-def create_nodes(input_file):
+def create_adj_list(input_file):
 
     adj_list = {}
 
@@ -17,3 +19,36 @@ def create_nodes(input_file):
                 adj_list[row['Adjacents']] = {row['Node ']}
 
     return adj_list
+
+
+# Initialize Map
+def initialize_map(filename): 
+    f = open(filename) 
+    data = json.load(f) 
+    nodes = []
+    for _,node in data.items(): 
+      temp_node = Node(
+        number = node["number"],
+        name = node["name"],
+        x = node["x"],
+        y = node["y"],
+        node_type = node["type"],
+        floor = node["floor"],
+        building = node["building"]
+      )
+      nodes.append(temp_node)
+    return nodes
+  
+
+if __name__ == "__main__":
+    dump_file = 'nodes.json'
+    nodes = initialize_map(dump_file)
+    csv_file = 'edges.csv'
+    adj_list = create_adj_list(csv_file) 
+    print(adj_list)
+    # Initialize Source/Destination Nodes
+    # Call Dijikstra/A* to get sequence of nodes
+    # Call Directions Function 
+    # return directions output
+
+
