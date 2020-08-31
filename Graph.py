831f4fb4e409ffe093f444db1d9e2e06afb9e665
revathivijay <34963337+revathivijay@users.noreply.github.com>
2020-08-31 21:15:43 +0530

    updated --

--- Graph.py
+++ Graph.py
@@ -6,6 +6,16 @@ import json
 import re
 from Node import Node
 from math import sqrt
+from time import sleep
+
+"""
+{0: Main Gate , 1: , 2: Main Building Entrace, 3: , 4: Main Building Staircase, 5: Director's Office,
+6: Lab 3, 7: Dep1 , 8: Dep2 , 9: , 10: Computer Department, 11: Study Space , 12: , 13: AL004 ,
+14: , 15: Stage , 16: , 17: Audi Entrance, 18: Stage Washroom, 19: Canteen Quad Entrance,
+20: Quad , 21: Quad Steps, 22: , 23: , 24: , 25: , 26: CCF1, 27: Library,
+28: Library Staircase, 29: COE, 30: , 31: Electrical Dept/Staircase, 32: Statue, 33: Quad Entrance}
+
+"""
 
 
 def initialize_map(filename):
@@ -66,6 +76,49 @@ class Graph():
         print()
         return path
 
+    def getDirections(self, path):
+        directions = []
+        # print("In directions: ", path)
+        for i in range(1, len(path)):
+            ## case 1: for first node - only parent is considered
+            if i==1:
+                curr = path[i]
+                prev = path[i-1]
+                if self.nodes[curr].x > self.nodes[prev].x and self.nodes[curr].y == self.nodes[prev].y:
+                    directions.append('Right')
+                elif self.nodes[curr].x < self.nodes[prev].x and self.nodes[curr].y == self.nodes[prev].y:
+                    directions.append('Left')
+                elif self.nodes[curr].x == self.nodes[prev].x and self.nodes[curr].y > self.nodes[prev].y:
+                    directions.append('Up')
+                elif self.nodes[curr].x == self.nodes[prev].x and self.nodes[curr].y < self.nodes[prev].y:
+                    directions.append('Down')
+            else:
+                x_curr, y_curr = self.nodes[path[i]].x, self.nodes[path[i]].y
+                x_prev, y_prev = self.nodes[path[i-1]].x, self.nodes[path[i-1]].y
+                x_prev_prev, y_prev_prev = self.nodes[path[i-2]].x, self.nodes[path[i-2]].y
+                # print("current: ", f'{x_curr}, {y_curr}')
+                # print("Prev: ", f'{x_prev}, {y_prev}')
+                # print("Prev prev: ", f'{x_prev_prev}, {y_prev_prev}')
+                if y_prev==y_curr==y_prev_prev and ((x_curr > x_prev and x_prev > x_prev_prev) or (x_curr < x_prev and x_prev < x_prev_prev)):
+                    directions.append('Straight')
+                elif x_prev==x_curr==x_prev_prev and ((y_curr > y_prev and y_prev > y_prev_prev) or (y_curr < y_prev and y_prev < y_prev_prev)):
+                    directions.append('Straight')
+                elif abs(x_curr-x_prev)<=5 and abs(x_prev-x_prev_prev)<=5 and (abs(y_prev_prev-y_prev)<=5 and y_prev > y_curr):
+                    directions.append("Right")
+                elif abs(x_curr-x_prev)<=5 and x_prev > x_prev_prev and (abs(y_prev_prev-y_prev)<=5 and y_prev < y_curr):
+                    directions.append("Left")
+                elif abs(x_curr-x_prev)<=5 and x_prev < x_prev_prev and (abs(y_prev_prev-y_prev)<=5 and y_prev > y_curr):
+                    directions.append("Left")
+                elif abs(x_curr-x_prev)<=5 and x_prev < x_prev_prev and (abs(y_prev_prev-y_prev)<=5 and y_prev < y_curr):
+                    directions.append("Right")
+                elif x_curr > x_prev and abs(x_prev-x_prev_prev)<=5 and (abs(y_prev-y_curr)<=5 and y_prev > y_curr):
+                    directions.append("Left")
+                elif x_curr < x_prev and abs(x_prev-x_prev_prev)<=5 and (abs(y_prev-y_curr) and y_prev > y_curr):
+                    directions.append("Right")
+
+        return directions
+
+
     def dijkstra(self, src, dest):
         V = self.V
         dist = []
@@ -98,36 +151,42 @@ class Graph():
                         parents[v] = u
                         minHeap.decreaseKey(v, dist[v])
         path = self.getSolution(dist, parents, source, dest)
-        return round(dist[dest], 2), path
+        directions = self.getDirections(path)
+        return round(dist[dest], 2), path, directions
 
     #TODO: add directions switch case
+
+
 
 ## driver code to test
 if __name__=='__main__':
 
-    source = 19
-    dest = 0
+    source = 0
+    dest = 18 #20, 19, 27, 30
 
-    ##TODO: code to redirect to nearest staircase - pseudo code written
+    #TODO: code to redirect to nearest staircase - pseudo code written
     # if nodes[dest].floor==1:
     #     for node in adj_list of dest
     #         if re.find('staircase') in nodes[dest].name:
     #             dest = that node
 
     nodes = initialize_map('nodes.json')
-    print(nodes)
+    # print(nodes)
 
     graph = Graph(34, nodes)
     graph.addAllEdges('edges.csv')
-    distance, path = graph.dijkstra(source, dest)
+    distance, path, directions = graph.dijkstra(source, dest)
 
-    print("Source: ", source, " ", nodes[source].name)
-    print("Destination: ", dest, " ", nodes[dest].name)
+    print("Source: ", nodes[source].name)
+    print("Destination: ", nodes[dest].name)
+    # print("Directions: ", directions)
     print("Path: ", path)
     for i in range(len(path)):
         if nodes[path[i]].name=="":
-            continue
+            print(" turning/junction ", end='')
         if i!=len(path)-1:
             print(nodes[path[i]].name, end=" -- > ")
         else:
             print(nodes[path[i]].name)
+    # for i in range(len(path)):
+    #     print(f"x: {nodes[path[i]].x}, y: {nodes[path[i]].y}")
